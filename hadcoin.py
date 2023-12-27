17c453d9df0a6f8bcaf2573af1976b1977ea3ea1
Durgesh Nandan <durgnan45@gmail.com>
2023-12-27 19:07:27 +0000

    Boilerplate code of hadcoin

--- hadcoin.py
+++ hadcoin.py
@@ -14,15 +14,18 @@ from urllib.parse import urlparse
 class Blockchain:
     def __init__(self):
         self.chain = []
-        self.create_block(proof = 1, previous_hash ='0')
+        self.transactions = []
+        self.create_block(proof=1, previous_hash='0')
+        self.nodes = set()
 
     def create_block(self, proof, previous_hash):
-        block = { 'index' : len(self.chain) + 1,
-                      'timestamp' : str(datetime.datetime.now()),
-                      'proof' : proof,
-                      'previous_hash' : previous_hash
-        }
-
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash,
+                 'transactions': self.transactions
+                 }
+        self.transactions = []
         self.chain.append(block)
         return block
 
@@ -33,7 +36,7 @@ class Blockchain:
         new_proof = 1
         check_proof = False
         while check_proof is False:
-            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            hash_operation = hashlib.sha256(str(new_proof ** 2 - previous_proof ** 2).encode()).hexdigest()
             if hash_operation[:4] == '0000':
                 check_proof = True
             else:
@@ -61,6 +64,33 @@ class Blockchain:
             block_index += 1
         return True
 
+    def add_transaction(self, sender, receiver, amount):
+        self.transactions.append({'sender': sender, 'receiver': receiver, 'amount': amount})
+        previous_block = self.get_previous_block()
+        return previous_block['index'] + 1
+
+    def add_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def replace_chain(self):
+        network = self.nodes
+        longest_chain = None
+        max_length = len(self.chain)
+        for node in network:
+            response = requests.get(f'http://{node}/get_chain')
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+                if(length > max_length and self.is_chain_valid(chain)):
+                    max_length = length
+                    longest_chain = chain
+        if longest_chain:
+            self.chain = longest_chain
+            return True
+        return False
+
+
 # Part 2 - Mining our Blockchain
 
 # Create Web app
@@ -68,6 +98,7 @@ app = Flask(__name__)
 
 # Creating a Blockchain
 blockchain = Blockchain()
+
 
 # Mining a new block
 @app.route('/mine_block', methods=['GET'])
@@ -85,11 +116,13 @@ def mine_block():
 
     return jsonify(response), 200
 
+
 # Getting the full blockchain
 @app.route('/get_chain', methods=['GET'])
 def get_chain():
     response = {'chain': blockchain.chain, 'length': len(blockchain.chain)}
     return jsonify(response), 200
+
 
 # Check if a blockchain is valid
 @app.route('/is_valid', methods=['GET'])
@@ -102,6 +135,10 @@ def is_valid():
         response = {'message': 'All good. The Blockchain is valid!'}
 
     return jsonify(response), 200
+
+
+# Part - 3 Decentralising the Blockchain
+
 
 # Running the app
 app.run(host='0.0.0.0', port=5001)
